9ecb90ae1d4e8d4d55e00d1e967271773d7287f1
Votre nom ou pseudo <lel.julien@laposte.net>
2020-04-16 10:28:26 +0200

    un test

--- src/main/java/interfaces/Information.java
+++ src/main/java/interfaces/Information.java
@@ -2,8 +2,13 @@ package interfaces;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface Information extends Remote{
 	public String getInformation() throws RemoteException;
+
+	public String getManger(String verbe) throws RemoteException;
+	
+	public ArrayList<String> getTraduction(String verbe, String Temps) throws RemoteException;
 
 }
--- src/main/java/rmi/rmiClientProject/App.java
+++ src/main/java/rmi/rmiClientProject/App.java
@@ -2,6 +2,8 @@ package rmi.rmiClientProject;
 
 import java.rmi.Naming;
 import java.rmi.Remote;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import interfaces.Information;
 
@@ -14,13 +16,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Lancement du client!" );
+        Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Veuillez entrer un mot :");
+		String str = sc.nextLine();
         try {
-			Remote r = Naming.lookup("rmi://192.168.0.24/TestRMI");
+			Remote r = Naming.lookup("rmi://192.168.0.17/TestRMI");
 			System.out.println(r);
 			if (r instanceof Information)
 			{
-				String s = ((Information) r).getInformation();
-				System.out.println("chaine renvoyee = "+s);
+				ArrayList<String> verbeConj = ((Information) r).getTraduction(str, "futur");
+				verbeConj.forEach(s -> System.out.println(s));
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
